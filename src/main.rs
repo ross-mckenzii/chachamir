48d62f8420d15a03a8ed33c7e5ca69eb1f25f652
Ross McKenzie <ross_mckenzie@protonmail.com>
2022-03-07 13:40:03 +0000

    Add placeholder icon and build script

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+extern crate shamir;
+extern crate chacha20poly1305;
+
+use std::io;
+use std::fs::File;
+use std::io::prelude::*;
+
+use chacha20poly1305::{ChaCha20Poly1305, Key, Nonce};
+use chacha20poly1305::aead::{Aead, NewAead};
+
+use shamir::SecretData;
+
 fn main() {
     println!("Hello, world!");
 }
