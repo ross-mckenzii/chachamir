5c530ee2b8898d0ec2d19b233c16b2440aeb55f9
Ross McKenzie <ross_mckenzie@protonmail.com>
2022-04-30 18:35:55 +0100

    Cleaned up path code, added CLI enforcement via Clap

--- src/main.rs
+++ src/main.rs
@@ -41,11 +41,11 @@ struct Arguments {
     file: PathBuf,
 
     // Encrypt file
-    #[clap(short, long, takes_value = false)]
+    #[clap(short, long, takes_value = false, conflicts_with = "decrypt", required_unless_present = "decrypt")]
     encrypt: bool,
 
     // Decrypt file
-    #[clap(short, long, takes_value = false)]
+    #[clap(short, long, takes_value = false, conflicts_with = "encrypt", required_unless_present = "encrypt")]
     decrypt: bool,
 
     /// Total number of shares to generate (max 65,535)
@@ -57,13 +57,14 @@ struct Arguments {
     threshold: Option<u16>,
 
     /// Path to the folder containing shares, or to write shares to (defaults to current working dir)
-    #[clap(parse(from_os_str), short)]
+    #[clap(parse(from_os_str), short, long)]
     shares: Option<PathBuf>,
 }
 
 // ---------
 // constants
 // ---------
+// version
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 // file header prefixes
@@ -91,10 +92,44 @@ fn stringify_path(path: &PathBuf) -> String { // turn path into string for print
     str_path
 }
 
+fn get_paths(cli_args: Arguments) -> [PathBuf; 2] { // Returns the file for encryption and folder for shares
+
+    // Get target file from path buffer
+    let target_file = PathBuf::from(&cli_args.file);
+    println!("[+] File: {}", stringify_path(&target_file) );
+
+    // Get shares directory (default to current working dir)
+    let shares_dir = match cli_args.shares {
+        Some(val) => val, // directory provided
+        None => { // default to working dir
+            println!("[+] Shares directory not provided... using current working directory");
+            let default_dir = env::current_dir().unwrap();
+
+            println!("");
+            println!("[#] Would you like to continue,");
+            println!("[#] using {} as the share directory?", stringify_path(&default_dir) );
+            println!("[#] (Ctrl+C to abort; provide path to use that instead; empty for default)");
+
+            // Wait for user confirmation
+            let mut confirm = String::new();
+            io::stdin().read_line(&mut confirm).expect("[!] Critical error with input");
+            
+            if !confirm.is_empty() {
+                PathBuf::from(confirm)
+            } else {
+                default_dir
+            }
+        }
+    };
+
+    let paths: [PathBuf; 2] = [target_file, shares_dir];
+    paths
+}
+
 fn read_file(filepath: &Path) -> Vec<u8> { // Raw function for reading files
     let mut contents = vec![];
-    fs::File::open(&filepath).unwrap().read_to_end(&mut contents);
-    
+    let result = fs::File::open(&filepath).unwrap().read_to_end(&mut contents).expect("Error reading file");
+
     contents
 }
 
@@ -129,53 +164,25 @@ fn main() {
     let args = Arguments::parse();
     logo(); // print logo
 
-    if args.encrypt && args.decrypt { // attempting to encrypt and decrypt at the same time
-        eprintln!("[!] Cannot encrypt and decrypt a file at the same time!");
-        process::exit(1);
-    }
-    else if args.encrypt { // Encryption
+    if args.encrypt { // Encryption
         println!("[*] Chose to encrypt a file...");
         
-        // Get target file from path buffer
-        let target_file = PathBuf::from(&args.file );
-        println!("[+] File: {}", stringify_path(&target_file) );
-
-        // Get shares directory (default to current working dir)
-        let mut shares_dir = match args.shares {
-            Some(val) => val, // directory provided
-            None => { // default to working dir
-                println!("[+] Shares directory not provided... using current working directory");
-                let default_dir = env::current_dir().unwrap();
-
-                println!("");
-                println!("[#] Would you like to continue,");
-                println!("[#] using {} as the share directory?", stringify_path(&default_dir) );
-                println!("[#] (Ctrl+C to abort; provide path to use that instead; empty for default)");
-
-                // Wait for user confirmation
-                let mut confirm = String::new();
-                io::stdin().read_line(&mut confirm).expect("[!] Critical error with input");
-                
-                if !confirm.is_empty() {
-                    PathBuf::from(confirm)
-                } else {
-                    default_dir
-                }
-                
-            }
-        };
+        let paths = get_paths(args);
+        let target_file = &paths[0];
+        let shares_dir = &paths[1];
 
         // print share dir being used
-        println!("[+] Storing shares at {}", absolute_path(shares_dir).unwrap().into_os_string().into_string().unwrap() );
+        println!("[+] Storing shares at {}", stringify_path(&shares_dir) );
     }
     else if args.decrypt { // Decryption
         println!("[*] Chose to decrypt a file...");
-    }
-    else {
-        println!("[#] You must choose to either encrypt or decrypt a file");
-        println!("[#] Encrypt with flag -e or --encrypt");
-        println!("[#] Decrypt with flag -d or --decrypt");
-        process::exit(0);
+
+        let paths = get_paths(args);
+        let target_file = &paths[0];
+        let shares_dir = &paths[1];
+
+        // print share dir being used
+        println!("[+] Shares folder: {}", stringify_path(&shares_dir) );
     }
 
     process::exit(0);
